afed0e86161a084eb770b57d4d77eabd19bd4ac0
kkkkkse <eun2abc@gmail.com>
2023-08-09 14:36:47 +0900

    [chore] test id constant 파일 생성

---
+++ src/constants/testId.ts
@@ -0,0 +1,11 @@
+export const TEST_ID_ROUTES = {
+  SIGNUP: 'signup-page',
+  SIGNIN: 'signin-page',
+  TODO: 'todo-page',
+};
+
+export const TEST_ID_SIGNUP = {
+  EMAIL: 'email-input',
+  PASSWORD: 'password-input',
+  SUBMIT_BUTTON: 'signup-button',
+};
